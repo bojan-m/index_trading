fad360075ce949535d4f1caf702aa6f6ddd33388
bojan-m <bojan.markovic212@gmail.com>
2018-09-13 14:27:15 +0200

    using itertools

--- indexingScrip.py
+++ indexingScrip.py
@@ -1,38 +1,21 @@
 import json
-import itertools
 
 data_json = open('data.json').read()
 data = json.loads(data_json)
 data_dict = data['data']
 
-gross_diff = 0.0
-buy_price = 0
-sell_price = 0
-buy_date = 0
-sell_date = 0
+previous = []
+gross = None
+gross_date = []
 
-list = []
 for i in data_dict[::-1]:
-    list.append([i['quote_date'],i['low'],i['high']])
+    if gross is None:
+        gross_date.append([i['quote_date'], i['high']-i['low']])
+        previous = i
+        gross = i['high']-i['low']
+    else:
+        gross_date.append([i['quote_date'], i['high'] - previous['low']])
+        previous = i
 
-final_list = []
-for a, b  in itertools.combinations(list, 2):
-    final_list.append([a,b])
 
-for i in final_list:
-    # # initialize
-    if buy_price == 0:
-        gross_diff = i[0][2] - i[0][1]
-        buy_price = i[0][1]
-        sell_price = i[1][2]
-        buy_date = i[0][0]
-        sell_date = i[1][0]
-    elif gross_diff < (i[1][2] - i[0][1]):
-        buy_price = i[0][1]
-        sell_price = i[1][2]
-        buy_date = i[0][0]
-        sell_date = i[1][0]
-        gross_diff = sell_price - buy_price
-
-print(buy_price, buy_date, sell_price, sell_date, gross_diff)
-
+print(gross_date)
